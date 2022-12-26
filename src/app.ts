3b1a0bc5138ae7a642a0faf286895f4b60ff4c22
Leandro Assis <leandrou9@gmail.com>
2022-12-26 10:10:41 -0300

    Criando interações com elementos DOM

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjetoEntrada {
     elementoTemplate: HTMLTemplateElement;
     elementoHost: HTMLDivElement;
     elemento: HTMLFormElement;
+    elementoTituloEntrada: HTMLInputElement;
+    elementoDescricaoEntrada: HTMLInputElement;
+    elementoPessoasEntrada: HTMLInputElement;
     
     constructor() {
         //importando componentes direto do index.html (devem ser declarados e tipados antes do constructor)
@@ -10,7 +13,23 @@ class ProjetoEntrada {
 
         const nodeImportado = document.importNode(this.elementoTemplate.content, true);
         this.elemento = nodeImportado.firstElementChild as HTMLFormElement;
+        this.elemento.id = 'user-input';    //id do app.css
+
+        this.elementoTituloEntrada = this.elemento.querySelector('#title') as HTMLInputElement;
+        this.elementoDescricaoEntrada = this.elemento.querySelector('#description') as HTMLInputElement;
+        this.elementoPessoasEntrada = this.elemento.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.anexo(); 
+    }
+
+    private controladorEnvio(event: Event) {
+        event.preventDefault();
+        console.log(this.elementoTituloEntrada.value);
+    }
+
+    private configure() {
+        this.elemento.addEventListener('submit', this.controladorEnvio.bind(this));
     }
     
     private anexo() {
